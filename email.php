68869cd78da72372ae6701afdb7af2ed2030ff2a
Pyae Phyo Maung <145741411+PyaePhyoMaung7@users.noreply.github.com>
2023-09-22 18:21:57 +0630

    Update email.php

--- email.php
+++ email.php
@@ -2,9 +2,5 @@
 
 echo "sending email to Mg Mg";
 echo "email sent to Su Su";
-echo "email sent to Myo Myo";
-echo "email sent to Zaw Zaw";
-echo "fifth line from online";
-echo "fifth line from cli";
 
 ?>
